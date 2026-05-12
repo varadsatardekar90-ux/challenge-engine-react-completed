e825a0c921ac26d90fd7a4e47acb72e508b3706a
varadsatardekar90-ux <vsatardekar60@gmail.com>
2026-05-12 14:29:58 +0530

    changes made

--- courses/03-nextjs-app-router/project/app/layout.tsx
+++ courses/03-nextjs-app-router/project/app/layout.tsx
@@ -62,35 +62,3 @@
 //   )
 // }
 
-// appDirectory fileBasedRouting serverComponent useClient useState dynamicExport forceStaticOrDynamic metadata generateMetadata nextFont nextImage useSelector useDispatch storeProvider
-import Link from 'next/link'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import StoreProvider from './providers/StoreProvider'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'Next.js App',
-  description: 'A Next.js App Router application',
-  openGraph: {
-    title: 'Next.js App',
-    description: 'A Next.js App Router application',
-  },
-}
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <nav>
-          <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
-        </nav>
-        <StoreProvider>
-          {children}
-        </StoreProvider>
-      </body>
-    </html>
-  )
-}
